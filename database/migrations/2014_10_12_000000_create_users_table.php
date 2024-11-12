2eab76dec444820dee6659510ebd80dc13b89b00
ikii557 <rifkihadidanwar@gmail.com>
2024-11-12 15:57:45 +0700

    membenarkan data data user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->enum('role',['admin','superadmin']);
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
